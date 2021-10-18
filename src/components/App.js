5b4ba710c42b0b303dedeecbcdc13fe30ee2e0c7
Devmmanuel <plasmmadev@gmail.com>
2021-10-18 00:11:50 -0500

    solucion de algunos conflictos

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { NavBar } from './NavBar'
 import { Portada } from './Portada'
-import { Home } from './Paginas/Home'
-//import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-//import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import Footer from './Footer';
 import Latests from './Latests';
-import Background from './Background';
 import Populars from './Populars';
 
 function App() {
   return (
     <>
-      <div className="App">
-          <Router>
-          <NavBar/>
-          <Portada/>
-          </Router>
-      </div>
-        <Background />
-        <Populars />
-        <Latests />
-        <Footer />
+      <Router>
+      <NavBar/>
+      <Portada/>
+      </Router>
+      <Populars />
+      <Latests />
+      <Footer />
     </>
   );
 }
