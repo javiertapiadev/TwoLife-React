6fd55e963a2ffe1d0f02142c492cf7dc0894e708
ivanDK5 <ing.iglds@gmail.com>
2021-11-28 13:50:19 -0600

    feat:Added modify styles for view login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,38 @@
 import { Box } from "@mui/system"
 import LoginForm from '../components/login/LoginForm'
+import { ReactComponent as PinkBlur } from '../assets/pink-blur.svg'
+import { ReactComponent as BlueBlur } from '../assets/blue-blur.svg'
+
+const styles = {
+  main: {
+      backgroundColor: "#0F2026",
+      minWidth: "100%",
+      minHeight: "100vh",
+      paddingTop: "20px",
+      paddingBottom: "30px",
+      position: "relative"
+  },
+  pink: {
+      position: "absolute",
+      top: "0",
+      right: "0",
+      maxHeight: "370px",
+      width: "auto"
+  },
+  blue: {
+      position: "absolute",
+      bottom: "0",
+      left: "0",
+      maxHeight: "270px",
+      width: "auto",
+      maxWidth: "100%"
+  }
+}
+
 
 function Login(){
   return(
-    <Box sx={{display:'flex',backgroundColor:'black',flexDirection:{xs:'column',lg:'row'}}}>
+ /*    <Box sx={{display:'flex',backgroundColor:'black',flexDirection:{xs:'column',lg:'row'}}}>
       <Box sx={{display:{xs:'none',lg:'flex'}, width:{lg:'50%'} , height:'100vh',backgroundColor:'pink'}}>
         hrhrhrhhr
       </Box>
@@ -13,7 +42,16 @@ function Login(){
         </div>
         <LoginForm></LoginForm>
       </Box>
-    </Box>
+    </Box> */
+
+    <>
+      <main style={styles.main}>
+          <PinkBlur style={styles.pink} />
+          <BlueBlur style={styles.blue} />
+          <LoginForm />
+      </main>
+     
+    </>
   )
 }
 
