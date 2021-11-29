83c1017c53fe90442cd8e30949b7c0904f1732b9
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2021-11-29 04:55:37 -0600

    Preparation for merge into rcarreno

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React , {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '../views/Home';
-import Compras from '../views/Compras';
 import RequestPage from '../views/RequestPage';
 import Ads from '../views/Ads';
-import Populars from './home/Populars';
 import Sell from '../views/Sell';
 import SignUp from '../views/SignUp';
 
@@ -17,9 +15,8 @@ function App() {
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/sell" component={Sell} />
           <Route exact path="/myrequests" component={RequestPage} />
-          <Route exact path="/myads" component={Ads} />
-          <Route exact path="/all-videogames" component={Populars} />
-          <Route exact path="/compras" component={Compras} />
+          {/* <Route exact path="/myads" component={} /> */}
+          <Route exact path="/ads" component={Ads} />
         </Switch>
       </Router>
     </>
