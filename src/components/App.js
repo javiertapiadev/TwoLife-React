dde20c3c93c9b09475d3b8f3e4417f72a8a623d0
ivanDK5 <ing.iglds@gmail.com>
2021-12-06 20:29:30 -0600

    feat:refactor routes using v6 and outlet

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,12 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Layout from './layout/Layout';
-import {
-  Home,
-  RequestPage,
-  Ads,
-  Sell,
-  SignUp,
-  IndividualAd,
-  Login
-} from '../views';
-
-const WrappedRoutes = () => {
-  return (
-    <Layout>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/sell" component={Sell} />
-      <Route exact path="/ads" component={Ads}/>
-      <Route exact path="/ads/:id" component={IndividualAd} />
-      <Route exact path="/myrequests" component={RequestPage} />
-      {/* <Route exact path="/myads" component={} /> */}
-    </Layout>
-  )
-}
-
+import { Outlet } from "react-router-dom";
+import NavBar from "./layout/NavBar";
+import Footer from "./layout/Footer";
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-          <Route component={WrappedRoutes}/>
-        </Switch>
-      </Router>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
     </>
   );
 }
